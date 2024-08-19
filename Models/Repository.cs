fd6d8c5bb70baff78755cf20aaf4033a2ddcb48c
Harun Selçuk Çetin <56540582+selcukctn@users.noreply.github.com>
2024-08-19 13:24:56 +0300

    first setup
    models and repository created

---
+++ Models/Repository.cs
@@ -0,0 +1,34 @@
+namespace FormsApp.Models
+{
+    public class Repository
+    {
+        private static readonly List<Product> _products = new();
+        private static readonly List<Category> _category = new();
+
+        static Repository() {
+            _category.Add(new Category {CategoryId = 1, Name = "Telefon"});
+            _category.Add(new Category {CategoryId = 2, Name = "Bilgisayar"});
+
+            _products.Add(new Product{ProductId=1, Name="Telefon Bir", Price=70000, Image="1.png", IsActive=true, CategoryId=1 });
+            _products.Add(new Product{ProductId=2, Name="Telefon İki", Price=70000, Image="2.png", IsActive=true, CategoryId=1 });
+            _products.Add(new Product{ProductId=3, Name="Bilgisayar Bir", Price=40000, Image="3.png", IsActive=true, CategoryId=2 });
+            _products.Add(new Product{ProductId=4, Name="Bilgisayar İki", Price=40000, Image="4.png", IsActive=true, CategoryId=2 });
+        }
+
+        public static List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
+
+        public static List<Category> Category
+        {
+            get
+            {
+                return _category;
+            }
+        }
+    }
+}
