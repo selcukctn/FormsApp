d3b3919aa5a9110e222f0f4e028653beddcf7682
Harun Selçuk Çetin <56540582+selcukctn@users.noreply.github.com>
2024-08-19 14:08:28 +0300

    search and filter
    search and filter products.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,29 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FormsApp.Controllers;
 
 public class HomeController : Controller
 {
-
-
-    public IActionResult Index()
+    public IActionResult Index(string search, string category)
     {
-        return View(Repository.Products);
+        var products = Repository.Products;
+
+        if (!string.IsNullOrEmpty(search))
+        {
+            ViewBag.search = search;
+            products = products.Where(p => p.Name.ToLower().Contains(search.ToLower())).ToList();
+        }
+        if (!string.IsNullOrEmpty(category) && int.Parse(category) != 0)
+        {
+            Console.WriteLine(category);
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+        }
+
+        ViewBag.Categories = new SelectList(Repository.Category, "CategoryId", "Name", category);
+        return View(products);
     }
 
     public IActionResult Privacy()
